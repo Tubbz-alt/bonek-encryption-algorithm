ae872386a8d7713f9d96d5264bee182edc696287
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-03-17 21:17:11 +0700

    Bonek algorithm done. Bug was eliminated

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
-#include <errno.h>
 
 #include "bonek_algorithm.h"
 #include "global.h"
 
 using namespace std;
+
+void print(Block b) {
+  for(int i = 0; i < BLOCK_SIZE; i++) {
+    printf("%d ", b.bit[i]);
+  }
+  printf("\n");
+}
+
+void print(vector<Block> b) {
+  for(auto it : b) print(it);
+}
+
+void print(vector<byte> b) {
+  for(auto it : b) printf("%d ", it); printf("\n");
+}
 
 int getFileSize(FILE* file) {
   int l = ftell(file);
@@ -15,29 +29,33 @@ int getFileSize(FILE* file) {
 }
 
 vector<byte> read_binary(char* name) {
-  printf("init %s length %d\n", name, strlen(name));
+  //printf("init %s length %d\n", name, strlen(name));
   vector<byte> plain;
-  printf("Reading binary file...\n");
-  FILE* file = fopen(name, "r");
-  printf("file %d\n", file);
-  printf("Error %d\n", errno);
+  //printf("Reading binary file...\n");
+  FILE* file = fopen(name, "rb");
+  //printf("file %d\n", file);
+  //printf("Error %d\n", errno);
   int fileSize = getFileSize(file);
   byte* filebuf = new byte[fileSize];
-  printf("get filesize done\n");
+  //printf("get filesize done\n");
   fread(filebuf, fileSize, 1, file);
   fclose(file);
-  printf("pindah ke filebuf done\n");
+  //printf("pindah ke filebuf done\n");
   for(int i = 0; i < fileSize; i++) {
     plain.push_back((byte) filebuf[i]);
   }
+  delete file;
+  delete [] filebuf;
   return plain;
 }
 
 void flush_to_file(vector<byte> output, char* name) {
   FILE* file = fopen(name, "wb");
-  char* buffer = new char[output.size()];
+  byte* buffer = new byte[output.size()];
   for(int i = 0; i < output.size(); i++) buffer[i] = output[i];
   fwrite(buffer, sizeof(byte), output.size(), file);
+  delete file;
+  delete [] buffer;
 }
 
 /* validate key, return true if key is correct */
@@ -66,7 +84,10 @@ vector<Block> byte_to_block(vector<byte> b) {
   assert(b.size() % BLOCK_SIZE == 0);
   vector<Block> ret;
   for(int i = 0; i * BLOCK_SIZE < b.size(); i++) {
-    ret.push_back(Block(8, reinterpret_cast<byte*> (&b[i])));
+    ret.push_back(Block(8));
+    for(int j = 0; j < BLOCK_SIZE; j++) {
+      ret[i].bit[j] = b[i * BLOCK_SIZE + j];
+    }
   }
   return ret;
 }
@@ -81,11 +102,10 @@ vector<byte> block_to_byte(vector<Block> b) {
   return ret;
 }
 /*
-e enc C:\Users\Luqman%20A.%20Siswanto\Code\bonek-encryption-algorithm/in.pdf out.pdf 00000aaaaa00000aaaaa00000aaaaafc
+e enc in.cpp out.cpp 00000aaaaa00000aaaaa00000aaaaafc
 */
 int main(int argc, char** argv)
 {
-  errno = 0;
   if(argc != 5) {
     puts("Usage : main [enc/dec][file_masuk][file_keluar][key]");
     exit(0);
@@ -102,21 +122,30 @@ int main(int argc, char** argv)
   printf("hexa to binary key done\n");
   
   vector<byte> v = read_binary(file_in);
-  puts("Reading binary done");
+  printf("Reading binary done. Original filesize is %d bytes\n", v.size());
   if(v.size() % BLOCK_SIZE) {
-    int add = BLOCK_SIZE - v.size() % 16;
+    int add = BLOCK_SIZE - v.size() % BLOCK_SIZE;
     printf("Adding %d bytes\n", add);
     while(add--) v.push_back(0);
+  } else {
+    printf("No byte added\n");
   }
   vector<Block> from = byte_to_block(v), to;
+  //printf("Before :\n"); print(from);
   
   Bonek bonek;
   if(string(argv[1]) == "enc") {
+    printf("Encrypting %d bytes with key %s\n", v.size(), argv[4]);
     to = bonek.encrypt(from, key);
+    puts("Encrypting success!");
   } else {
+    printf("Decrypting %d bytes with key %s\n", v.size(), argv[4]);
     to = bonek.decrypt(from, key);
+    puts("Decrypting success!");
   }
+  //printf("\nAfter :\n"); print(to);
   vector<byte> output = block_to_byte(to);
+  //printf("\nByte :\n"); print(output);
   flush_to_file(output, file_out);
   return 0;
 }
